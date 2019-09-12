86de0b26b33036c535331883494f56e76fa48378
Darrel C <dcastellano2007@gmail.com>
2019-09-12 19:41:02 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(string)
-   newsent = string.split(" ")
+  # newsent = string.split(" ")
 
   # newsent.each do |word|
   #   reverse_words << word.reverse
   # end
   # reverse_words.join(" ")
 
-  newsent.collect {|ele| ele.reverse }.join(" ")
+  string.split.collect {|ele| ele.reverse }.join(" ")
 
 end
